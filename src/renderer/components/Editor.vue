1a67f4d0df99d32b4220b57660ac5a9e5240d299
Masumi Kawasaki <mkawasaki@sbihg.co.jp>
2018-04-14 16:42:41 +0900

    Init

---
+++ src/renderer/components/Editor.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="editor">
+    <textarea :value="input" @input="update"></textarea>
+    <div v-html="compiledMarkdown"></div>
+  </div>
+</template>
+
+<script>
+import marked from 'marked'
+import _ from 'lodash'
+
+export default {
+  name: 'bob-editor',
+  data () {
+    return {
+      input: ''
+    }
+  },
+  computed: {
+    compiledMarkdown: function () {
+      return marked(this.input, { sanitize: true })
+    }
+  },
+  methods: {
+    update: _.debounce(function (e) {
+      this.input = e.target.value
+    }, 300)
+  }
+}
+</script>
+
+<style scoped>
+textarea, #editor div {
+  display: inline-block;
+  width: 49%;
+  height: 100%;
+  vertical-align: top;
+  box-sizing: border-box;
+  padding: 0 20px;
+}
+
+textarea {
+  border: none;
+  border-right: 1px solid #ccc;
+  resize: none;
+  outline: none;
+  background-color: #f6f6f6;
+  font-size: 14px;
+  font-family: 'Monaco', courier, monospace;
+  padding: 20px;
+}
+
+code {
+  color: #f66;
+}
+
+</style>
