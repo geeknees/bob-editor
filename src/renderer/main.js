ef787be3d5a374cdec8116f1a6d2c909f90b9d4e
mkawasaki <mkawasaki@sbihg.co.jp>
2018-05-20 01:32:10 +0900

    update

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -6,7 +6,11 @@ import router from './router'
 import store from './store'
 
 import Vuetify from 'vuetify'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
 Vue.use(Vuetify)
+Vue.use(ElementUI)
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
